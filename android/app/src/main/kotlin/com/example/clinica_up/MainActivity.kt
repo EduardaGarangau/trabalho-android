a1495b38d7dd51043ae78bd111988db2aef45f39
Eduarda Garangau <eduardagarangau98@gmail.com>
2022-11-21 13:46:38 -0300

    Criando Modelo do Teste

---
+++ android/app/src/main/kotlin/com/example/clinica_up/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clinica_up
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/teste_model.dart
@@ -0,0 +1,37 @@
+class Teste {
+  final int id;
+  final int quant;
+  final String nome;
+  final String sigla;
+  final String construto;
+  final String contexto;
+  final String idade;
+  final String aplicacao;
+  final String tempoDeAplicacao;
+  final String correcao;
+  final String validade;
+  final String objetivo;
+  final String publicoAlvo;
+  final String descricao;
+  final String itens;
+  final String profissionaisAplicantes;
+
+  Teste({
+    required this.id,
+    required this.quant,
+    required this.nome,
+    required this.sigla,
+    required this.construto,
+    required this.contexto,
+    required this.idade,
+    required this.aplicacao,
+    required this.tempoDeAplicacao,
+    required this.correcao,
+    required this.validade,
+    required this.objetivo,
+    required this.publicoAlvo,
+    required this.descricao,
+    required this.itens,
+    required this.profissionaisAplicantes,
+  });
+}
